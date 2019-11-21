a730081db2f89c3b9e85fc0c461c00b2518a8544
Julissa JOSEPH <54089454+julissa95@users.noreply.github.com>
2019-11-20 21:26:10 -0300

    m jus modifye AcesoDato nan RemoteMySql

--- hacerLogin.php
+++ hacerLogin.php
@@ -1,63 +1,43 @@
 <?php
-    
-$checkUsuario = $_GET['Usuario'];
-$checkPassword = $_GET['Contraseña'];
-	
-/*$Bandera=1;
+session_start();
+$checkUsuario = $_GET['Nombre'];
+$checkPassword = $_GET['Apellido'];
+$Bandera=1;
+
 $archivo = fopen("listadoregistro.txt", "r");
 while(!feof($archivo)) 
 	{
 		$objeto = json_decode(fgets($archivo));
+
 		if ($objeto->Apellido == $checkPassword) 
 		{
 			if($objeto->Nombre == $checkUsuario)				 	
 			{
 				$Bandera=0;
-				
-			
+				session_start();
+				$_SESSION['Nombre'] = $objeto->Nombre;
+			echo($_SESSION['Nombre'])
 			}
+
 		}
-				 	
+			
 		
 	}	
+
 if($Bandera==0)
 	{	
-		session_start();
-		$_SESSION['Nombre']=$checkUsuario;
-		header("Location: OK.php?login=exito");
+		header("Location: OK.php");
 		exit();
 	}
 	else 
 	{
+
 		header("Location: NO.php");
 		exit();
 				 	
 	}
-fclose($archivo); */
+fclose($archivo);
+		
 
-// Se conecta al SGBD 
-  if(!($iden =  mysqli_connect("localhost", "root", "","phpdatabase"))) 
-    die("Error: No se pudo conectar");
-	
-  // Selecciona la base de datos 
- 
-	
-  // Sentencia SQL: muestra todo el contenido de la tabla "books" 
-  $sentencia = "SELECT * FROM usuario where nombre='$checkUsuario' and clave='$checkPassword'"; 
-  // Ejecuta la sentencia SQL 
-  $resultado = mysqli_query($iden,$sentencia); 
-  $count = $resultado->num_rows;
-  if($count<=0) {
-		header("Location: login.php");
-		exit();
-  }else{
-  	session_start();
-		$_SESSION['Nombre']=$checkUsuario;
-		header("Location: OK.php?login=exito");
-		exit();
-  }
-    
-
- 	
 		
 ?>
--- hacerRegistro.php
+++ hacerRegistro.php
@@ -1,74 +1,39 @@
 <?php
     
-$checkUsuario = $_GET['Usuario'];
-$checkPassword = $_GET['Contrasena'];
-//var_dump($_GET);
-
-if(checkUser($checkUsuario,$checkPassword)==false){
-	// Create connection
-$conn = new mysqli("localhost", "root", "", "phpdatabase");
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
- 
-
- $sql = "insert into  usuario (Nombre, Clave) VALUES ('$checkUsuario','$checkPassword')"; 
- if ($conn->multi_query($sql) === TRUE) {
-   
-    header("Location: login.php");
-	exit();
-
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-    header("Location: Registro.php");
-  
-}
-
-$conn->close();
-}
-else {
-  
-    //
-      //phpAlert("Usuario ya registrado");
-     // header("Location: Registro.php");
-      echo "<script>
-            alert('Usuario Ya registrado!!!');
-            window.location.href='Registro.php';  
-            </script>";
-    }
-
-
-function checkUser($checkUsuario,$checkPassword) {
-
-	 if(!($iden =  mysqli_connect("localhost", "root", "","phpdatabase"))) 
-    	die("Error: No se pudo conectar");
-    // Sentencia SQL: muestra todo el contenido de la tabla "books" 
-  $sentencia = "SELECT * FROM usuario where nombre='$checkUsuario' and clave='$checkPassword'"; 
-  // Ejecuta la sentencia SQL 
-  $resultado = mysqli_query($iden,$sentencia);
-  //var_dump($resultado);
-  //die(); 
-  $count = $resultado->num_rows;
-  if($count<=0) {
-		return false;
+$checkUsuario = $_GET['Nombre'];
+$checkPassword = $_GET['Apellido'];
+	
+$Bandera=1;
+$archivo = fopen("listadoregistro.txt", "r");
+while(!feof($archivo)) 
+	{
+		$objeto = json_decode(fgets($archivo));
+		if ($objeto->Apellido == $checkPassword) 
+		{
+			if($objeto->Nombre == $checkUsuario)				 	
+			{
+				$Bandera=0;
+				
+			
+			}
+		}
+				 	
+		
+	}	
+if($Bandera==0)
+	{	
+		session_start();
+		$_SESSION['Nombre']=$checkUsuario;
+		header("Location: OK.php?login=exito");
+		exit();
 	}
-	else{
-		return true;
+	else 
+	{
+		header("Location: NO.php");
+		exit();
+				 	
 	}
-	$iden->close();
-}
-function phpAlert($msg) {
-  echo "window.location.href = '/julissa/Registro.php'";
-  echo '<script type="text/javascript">alert("' . $msg . '")</script>';
-  //echo "<script type='javascript/text'>";
-  //echo "alert('$msg')";
-  //echo "window.location.href = '/julissa/Registro.php'";
-  //echo "</script>";
-}
-
-
-
-//if (check(3)) echo "Returned true!";
-
+fclose($archivo);
+		
+		
 ?>
